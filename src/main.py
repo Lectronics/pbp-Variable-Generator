85ae267c9c3e346704a91d7b39ad746d9678b3db
Ben <benzlectric@gmail.com>
2024-09-24 13:04:25 -0400

    Fiexd the accidental deletion of all of the files...

---
+++ src/main.py
@@ -0,0 +1,26 @@
+import GeneratorGUI as gui
+import BMF_Tab
+import BMF_GUI
+import Menubar
+from tkinter import Tk, ttk
+
+root = Tk()
+root.geometry("650x800")
+
+tabbed_pages = ttk.Notebook(root)
+
+variable_tab = gui.VariableGenerator(master=tabbed_pages, root=root)
+
+# bmf_tab = BMF_Tab.BMFTab(master=tabbed_pages)
+bmf_gui = BMF_GUI.BMF(master=tabbed_pages)
+# bmf_tab.child = bmf_gui
+
+tabbed_pages.add(variable_tab, text="Variable Generator")
+tabbed_pages.add(bmf_gui, text="Bit Map Font")
+
+tabbed_pages.pack(expand=1, fill="both")
+
+# KeyBinds!!
+
+tabbed_pages.mainloop()
+
